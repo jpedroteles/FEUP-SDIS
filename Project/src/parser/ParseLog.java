fb180973e1363849be2b03c6f0199bc4a064e38f
José Pedro <josepedroteles@gmail.com>
2016-04-03 05:19:01 +0100

    Update ParseLog.java

--- Project/src/parser/ParseLog.java
+++ Project/src/parser/ParseLog.java
@@ -11,42 +11,76 @@ import java.util.stream.Stream;
 
 public class ParseLog {
 
+	/** Retorna o nome de um determinado ficheiro
+	* @param  line string onde esta o nome do ficheiro
+	* @return de uma string com o respetivo nome
+ 	*/
 	public String getFilename(String line){
 		String[] split = line.split(" ");
 		return split[0];
 	}
 
+	/** Retorna o identificador de um determinado ficheiro
+	* @param  line string onde esta o identificador do ficheiro
+	* @return de uma string com o respetivo identificador
+ 	*/
 	public String getFileId(String line){
 		String[] split = line.split(" ");
 		return split[1];
 	}
 
+	/** Retorna o numero de um determinado chunk
+	* @param  line string onde esta o numero do chunk
+	* @return de uma string com o respetivo numero
+ 	*/
 	public String getChunkNum(String line){
 		String[] split = line.split(" ");
 		return split[2];
 	}
 
+	/** Retorna o identificador do servidor
+	* @param  line string onde esta o identificador do servidor
+	* @return de uma string com o respetivo identificador
+ 	*/
 	public String getServerId(String line){
 		String[] split = line.split(" ");
 		return split[3];
 	}
 
+	/** Retorna o tipo de mensagem
+	* @param  line string onde esta o tipo de mensagem
+	* @return de uma string com o respetivo tipo
+ 	*/
 	public String getMessageType(String line){
 		String[] split = line.split(" ");
 		return split[4];
 	}
 
+	/** Retorna o tipo 
+	* @param  line string onde esta o tipo
+	* @return de uma string com o respetivo tipo
+ 	*/
 	public String getType(String line){
 		String[] split = line.split(" ");
 		return split[5];
 	}
 
+	/** Retorna uma linha do log com o historico de operacoes
+	* @param  line inteiro com o numero da linha
+	* @return de uma string com a linha respetiva
+ 	*/
 	public String getLine(int line) throws IOException{
 		Stream<String> lines = Files.lines(Paths.get("logs/history.txt"));
 		String ret = lines.skip(line-1).findFirst().get();
 		return ret;
 	}
 
+	/** Retorna numero de copias para um ficheiro
+	* @param  hash nome "hashado" do ficheiro
+	* @param chunkNum numero de chunk
+	* @param serverId identificador do servidor
+	* @return de um inteiro com o numero de copias
+ 	*/
 	public int countRepDegree(String hash, String chunkNum, String serverId) throws IOException{
 		int ret=0;
 		ArrayList<String> serverIds = new ArrayList<String>();
@@ -80,6 +114,10 @@ public class ParseLog {
 		return ret;
 	}
 
+	/** Retorna uma string com o nome do ficheiro
+	* @param  hash string com a hash do ficheiro
+	* @return de uma string com o respetivo nome
+ 	*/
 	public String getFile(String hash) throws IOException{
 
 		String ret = null;
