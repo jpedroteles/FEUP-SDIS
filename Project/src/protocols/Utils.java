08153b3ac9f00f6999b7a7ca88db57647d5f62ca
José Pedro <josepedroteles@gmail.com>
2016-04-03 13:34:24 +0100

    Update Utils.java

--- Project/src/protocols/Utils.java
+++ Project/src/protocols/Utils.java
@@ -4,6 +4,8 @@ import java.io.File;
 
 public class Utils {
 
+	/** Verifica se existe uma pasta chamada chunks se nao existir cria uma
+	*/
 	public void checkFolder() {
 
 		File folder = new File("chunks");
@@ -13,7 +15,10 @@ public class Utils {
 			folder.mkdir();
 		}
 	}
-
+	/** Verifica se existe um ficheiro com o nome de filename 
+	* @param filename nome do ficheiro
+	* @return true se encontrou o ficheiro false se nao
+	*/
 	public boolean checkFile(String filename){
 		
 		return new File("chunks", filename).exists();
