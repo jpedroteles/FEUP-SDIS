61f7bf0b9cc288983f76b684feb784ceda5d7251
José Pedro <josepedroteles@gmail.com>
2016-04-03 14:18:26 +0100

    Update MessageProcessor.java

--- Project/src/udp/MessageProcessor.java
+++ Project/src/udp/MessageProcessor.java
@@ -10,15 +10,29 @@ import protocols.Reclaim;
 import protocols.Restore;
 
 public class MessageProcessor implements Runnable {
-
+	/** Thread de tratamento da mensagem */
 	public Thread msg_thread;
+	/** Headder da mensagem */
 	public String header;
+	/** Conteudo da mensagem */
 	public byte[] content;
+	/** Parse da mensage */
 	public ParseMessage pm = new ParseMessage();
+	/** Identificador do servidor */
 	public String serverId;
+	/** Numero de porta multicast */
 	int port;
+	/** Adereco de porta multicast */
 	String address;
 
+	
+	/** Construtor do processador da mensagem dos canais udp
+	* @param h header da mensagem
+	* @param c conteudo da mensage
+	* @param ServerId Identificador do servidor
+	* @param p porta do canal de comunicacao
+	* @param a adereco do do canal de comunicacao
+ 	*/
 	public MessageProcessor(String h, byte[] c, String ServerID, int p, String a) {
 		port=p;
 		address=a;
@@ -29,6 +43,8 @@ public class MessageProcessor implements Runnable {
 		msg_thread.start();
 	}
 
+	/** Processa a mensagem recebida e inicializa os protocolos respetivos
+ 	*/
 	public void process_message() throws IOException, InterruptedException {
 		
 		String messageType = pm.getMessageType(header);
@@ -52,7 +68,9 @@ public class MessageProcessor implements Runnable {
 		default: break;
 		}
 	}
-
+	
+	/** Thread que gere o processamento da mensagem
+ 	*/
 	public void run() {
 
 		try {
