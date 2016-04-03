4bd60859727379492689bc58f6a8e1a8afd75d01
José Pedro <josepedroteles@gmail.com>
2016-04-03 14:34:06 +0100

    Update Multicast_Control.java

--- Project/src/udp/Multicast_Control.java
+++ Project/src/udp/Multicast_Control.java
@@ -9,19 +9,37 @@ import parser.ParseMessage;
 import protocols.History;
 
 public class Multicast_Control implements Runnable {
-
+	
+	/** Thread de tratamento do protocolo */
 	public static Thread mc_thread;
+	/** Numero de porta multicast control*/
 	public static int mc_port;
+	/** Adereco de porta multicast control*/
 	public static String mc_address;
+	/** Numero de porta multicast restore*/
 	public static int mdr_port;
+	/** Adereco de porta multicast restore*/
 	public static String mdr_address;
+	/** Socket multicast control*/
 	public static MulticastSocket mc;
+	/** Adereco de porta multicast control*/
 	public static InetAddress mcAddress;
+	/** Parse da mensagem*/
 	public ParseMessage pm = new ParseMessage();
+	/** Flag de fim*/
 	private static char crlf[] = {0xD,0xA};
+	/** Identificador do servidor*/
 	public String ServerID;
+	/** Novo historico*/
 	History hist = new History();
 
+	/** Inicializa o multicast de controlo
+	* @param ServerId mensagem a ser enviada
+	* @param mc_a adereco do canal controlo
+	* @param mc_p porta do canal controlo
+	* @param mdr_a adereco do canal restore
+	* @param mdr_p porta do canal restore
+ 	*/
 	public Multicast_Control(String ServerId, String mc_a, int mc_p, String mdr_a, int mdr_p) throws IOException{
 		mc_port=mc_p;
 		mc_address=mc_a;
@@ -36,6 +54,8 @@ public class Multicast_Control implements Runnable {
 		mc_thread.start();
 	}
 
+	/** Recebe uma mensagem do canal separa em header e conteudo, chama o processamento respetivo e adiciona ao historico
+ 	*/
 	public void mc_communication() throws IOException{
 
 		byte[] data= new byte[65536];
@@ -57,6 +77,8 @@ public class Multicast_Control implements Runnable {
 		MessageProcessor msg_pro = new MessageProcessor(header, content, ServerID, mdr_port, mdr_address);
 	}
 
+	/** Thread que gera o canal de comunicacao
+ 	*/
 	public void run() {
 		
 		while(true) {
