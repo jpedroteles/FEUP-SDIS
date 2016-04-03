3138842df6c78f5f2f52c0d98ef682fd8881191d
José Pedro <josepedroteles@gmail.com>
2016-04-03 13:31:07 +0100

    Update History.java

--- Project/src/protocols/History.java
+++ Project/src/protocols/History.java
@@ -12,7 +12,14 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
 public class History {
-
+	/** Adiciona uma operacao a um ficheiro com todos as operacoes que o servidor e cliente realizaram ou pediram
+	* @param  filename  nome do ficheiro
+	* @param  fileId  identificador do ficheiro
+	* @param  chunkNum numero do chunk
+	* @param  senderOrig servidor de origem
+	* @param  messageType tipo de mensagem
+	* @param  type se recebeu ou se enviou
+ 	*/
 	public void add(String filename, String fileId, String chunkNum, String serverOrig, String messageType, String type) throws IOException{
 
 		String name = "logs/history.txt";
